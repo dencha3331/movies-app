33b0a2a8021a0d3f638137132728cb084b6f0464
dencha3331 <garry3331@icloud.com>
2024-11-17 16:23:16 +0300

    add api v1 prefixes, authJWT, kinopoisk settings in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from pydantic import BaseModel
 from pydantic import PostgresDsn
 from pydantic_settings import (
@@ -5,14 +7,32 @@ from pydantic_settings import (
     SettingsConfigDict,
 )
 
+BASE_DIR = Path(__file__).parent.parent
+
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
 
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    auth: str = "/auth"
+    users: str = "/users"
+    kinopoisk: str = "/movies"
+
+
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+    @property
+    def bearer_token_url(self) -> str:
+        # api/v1/auth/login
+        parts = (self.prefix, self.v1.prefix, self.v1.auth, "/login")
+        path = "".join(parts)
+        # return path[1:]
+        return path.removeprefix("/")
 
 
 class DatabaseConfig(BaseModel):
@@ -31,6 +51,21 @@ class DatabaseConfig(BaseModel):
     }
 
 
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 15
+    refresh_token_expire_days: int = 30
+    lifetime_seconds: int = 3600
+    reset_password_token_secret: str
+    verification_token_secret: str
+
+
+class Kinopoisk(BaseModel):
+    api_key: str = ""
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(".env.template", ".env"),
@@ -41,6 +76,8 @@ class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
+    auth: AuthJWT = AuthJWT()
+    kinopoisk: Kinopoisk = Kinopoisk()
 
 
 settings = Settings()
